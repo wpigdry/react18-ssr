ef27c0d65cc5d76ad3ffda02ec1ea8fa45cd70ae
wpigdry <1070683378@qq.com>
2022-08-17 22:58:01 +0800

    node使用babel编译前端语法

--- react18-ssr/server.js
+++ react18-ssr/server.js
@@ -1,11 +1,15 @@
-const Koa = require('koa');
-const routerConfig = require('./routerConfig');
+/**
+ * @file ssr node服务启动文件
+ */
 
-const server = new Koa();
+// 底层改写了 node 的 require 方法, 运行时即时编译来支持 ES6 模块化语法
+const register = require('@babel/register');
+// @babel/preset-env  es语法编译
+// @babel/preset-react  编译react语法
+// @babel/plugin-transform-modules-commonjs 该插件将 ECMAScript 模块转换为CommonJS
+register({
+    ignore: [/node_modules/],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-transform-modules-commonjs'],
+});
 
-server.use(routerConfig.routes());
-
-const port = 5090;
-server.listen(port, () => {
-    process.stdout.write(`server listen: http://localhost:${port}\n`)
-})
